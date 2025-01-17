81c03066ac5481f0086c2fee275d45d4c4e75a4d
Andy <dkmin9392@gmail.com>
2025-01-17 17:57:00 +0900

    17th

--- workspace/dotori/src/day01/Example05.java
+++ workspace/dotori/src/day01/Example05.java
@@ -2,4 +2,16 @@ package day01;
 
 public class Example05 {
 
+	public static void main(String[] args) {
+
+		boolean result = hello(2);
+		
+		
+
+}
+	static boolean hello(int a) {
+		return a % 2 == 0? true : false;	
+	}
+	
+	
 }
